852a2d53f540ec92b2978d03c474ea2b8a94de56
松本 勝年 <k_matsumoto@k-matsumoto1.local>
2017-08-07 12:39:05 +0900

    Update

---
+++ python/test_calculator.py
@@ -0,0 +1,16 @@
+import pytest
+from calculator import Calculator
+
+#def pytest_funcarg__calc():
+@pytest.fixture()
+def calc():
+    return Calculator()
+
+@pytest.mark.parametrize("a, b, r", [(9, 8, 17), (1, 1, 2)])
+def test_add(calc, a, b, r):
+    assert calc.add(a, b) == r
+
+@pytest.mark.parametrize("a, b, r", [(9, 8, 1), (1, 1, 0)])
+def test_sub(calc, a, b, r):
+    assert calc.sub(a, b) == r
+
